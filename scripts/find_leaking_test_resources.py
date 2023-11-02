d0b6b373146978df2341cc6aa153e74a4febb3ca
Simon Walker <simon.walker@localstack.cloud>
2023-11-02 14:08:29 +0000

    Support sns topics

--- scripts/find_leaking_test_resources.py
+++ scripts/find_leaking_test_resources.py
@@ -42,6 +42,10 @@ METHOD_PAIRS = {
         "create": ["CreateBucket"],
         "delete": ["DeleteBucket"],
     },
+    "sns": {
+        "create": ["CreateTopic"],
+        "delete": ["DeleteTopic"],
+    },
     "stepfunctions": {
         "create": ["CreateStateMachine"],
         "delete": ["DeleteStateMachine"],
